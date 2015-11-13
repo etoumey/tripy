5114547dc0311f99d7d3125abe6fa8d2beb4e29b
Julian Toumey <jtoumey@gmail.com>
2015-11-12 22:12:42 -0600

    input parser goes string-by-string, started writing code to remove whitespace

--- src/parse_file_line.c
+++ src/parse_file_line.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+char *strip_spaces(char *str);
+
 /*************************************************************************/
 /*                                                                       */
 /*  File:         parse_file_line.c                                      */
@@ -17,15 +19,36 @@ int parse_file_line(char *ptr_file_name)
 {
    FILE *fp;
    char individual_line;
-   char buffer[100];
+   char buffer[256];
    size_t len;
+   char *ptr_b;
+   int counter = 1;
+
 
    fp = fopen("./TestGPX.gpx", "r");
    
-      
-   fgets(buffer, 256, fp);
+   while(fgets(buffer, 256, fp))
+   {
+      printf("Reading file...\n");
+      printf("Line: %d, %s\n",counter,buffer);
 
-   printf("%s\n",buffer);
+      ptr_b = strip_spaces(&buffer[256]);      
+      printf("%s\n",&ptr_b);
+      counter = counter++;
+   }
+   fclose(fp);
 
    return(0);
+}
+
+char *strip_spaces(char *str)
+{
+   char *end;
+
+   while(isspace(str[256]))
+   {
+       str++;
+   }
+
+   return(str);
 }
