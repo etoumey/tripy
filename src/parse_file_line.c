70a6508c9782d9051c39ff65389cb6d1df1aa1cd
Julian Toumey <jtoumey@gmail.com>
2015-11-23 22:16:37 -0600

    added a really rough hack-y method for splitting up the string

--- src/parse_file_line.c
+++ src/parse_file_line.c
@@ -29,6 +29,18 @@ int parse_file_line(char *ptr_file_name)
    char time_buffer_nc[6];
    int i,j;
 
+   // variables for converting string to int
+   long hour_ret;
+   long minute_ret;
+   long second_ret;
+   
+   char *ptr_conv;
+
+   char hour_char_type[2];
+   char minute_char_type[2];
+   char second_char_type[2];
+   //
+
    i = 0;
    j = 0;
 
@@ -53,20 +65,26 @@ int parse_file_line(char *ptr_file_name)
       if(strncmp(buffer,"<time",5) == 0)
       {
          printf(":)\n");
-         strncpy(time_buffer, buffer + 17, 8);
+         strncpy(time_buffer, buffer + 17, 6);
          printf("%s\n",time_buffer);
-/*         
-         while(time_buffer[i] != '\0')
-         {
-            if(time_buffer[i] != ':')
-            {
-               time_buffer[j++] = time_buffer[i];
-            }
-         } */
       }
+      // Now our time_buffer holds a 6-char string with hrs, minutes, seconds
+      // let's convert to seconds
+      // 
+      strncpy(hour_char_type, time_buffer, 2);
+      printf("%s\n",hour_char_type);
+      strncpy(minute_char_type, time_buffer + 2, 2);
+      printf("%s\n",minute_char_type);
+      strncpy(second_char_type, time_buffer + 4, 2);
+      printf("%s\n",second_char_type);
+//      hour_ret = strtol(time_buffer, &ptr_conv, 10);
+  //    printf("%ld\n",hour_ret);
 
       counter++;
    }
+
+
+   // Close input file stream
    fclose(fp);
 
    return(0);
@@ -81,8 +99,8 @@ char* strip_leading_spaces(char *input)
 
    while(input[i] != '\0')
    {
+      // check if current pointer location is not equal to a space and colon
       if(input[i] != ' ' && input[i] != ':')
-    //  if(input[i] != ' ')
       {
          input[j++] = input[i];
       }
