3c2de0dfabf4bdc05ddcd8104b3665031dfea0df
Julian Toumey <jtoumey@gmail.com>
2015-12-02 20:08:23 -0600

    Calc Stress: Added fcn to calculate stress

---
+++ src/calc_stress.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/*************************************************************************/
+/*                                                                       */
+/*  File:         calc_stress.c                                          */
+/*                                                                       */
+/*  Date:         December 2015                                          */
+/*                                                                       */
+/*  Description:  Use the time (in seconds) spent in each heart rate     */
+/*                to approximate the physiological stress                */
+/*                each <time> tag.                                       */
+/*                                                                       */
+/*  Inputs:                                                              */
+/*                                                                       */
+/*************************************************************************/
+
+void calc_stress(int *raw_time)
+{
+
+}
